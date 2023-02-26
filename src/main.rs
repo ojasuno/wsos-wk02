55f93a739a6996a8f12178c9d575de354f997677
ojasuno <ojasuno21@gmail.com>
2023-02-27 10:57:21 +1100

    add parse

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,18 @@ fn main() {
     println!("D : for Division");
     println!("E : for Exit");
 
+    // empty_string_zero
     let mut empty_string_zero = String::new();
     io::stdin()
         .read_line(&mut empty_string_zero)
         .expect("Failed to read line");
+    let digit_zero:String = empty_string_zero
+        .trim()
+        .parse()
+        .expect("Input not an integer");
     println!("empty_string_zero: {}", empty_string_zero);
 
+    // empty_string_one
     let mut empty_string_one = String::new();
     io::stdin()
         .read_line(&mut empty_string_one)
@@ -26,6 +32,7 @@ fn main() {
         .expect("Input not an integer");
     println!("empty_string_one: {}", empty_string_one);
 
+    // empty_string_two
     let mut empty_string_two = String::new();
     io::stdin()
         .read_line(&mut empty_string_two)
@@ -36,33 +43,31 @@ fn main() {
         .expect("Input not an integer");
     println!("empty_string_two: {}", empty_string_two);
 
-    // let op_result_zero = &digit_one + &digit_two;
-    // println!("main: Result of this operation is: {}", op_result_zero);
-    println!("STILL THERE -- empty_string_zero: {}", empty_string_zero);
+    // println!("STILL THERE -- empty_string_zero: {}", empty_string_zero);
 
-
-    if empty_string_zero == "A" {
-        println!("You picked -- {}", empty_string_zero);
+    // if empty_string_zero == "A" {
+    if digit_zero == "A" {
+        println!("You picked -- {}", digit_zero);
         let op_result_add = calc_add(digit_one, digit_two);
         println!("add: Result of this operation is: {}", op_result_add);    
     }
-    else if empty_string_zero == "S" {
-        println!("You picked -- {}", empty_string_zero);
+    else if digit_zero == "S" {
+        println!("You picked -- {}", digit_zero);
         let op_result_substract = calc_substract(digit_one, digit_two);
         println!("substract: Result of this operation is: {}", op_result_substract);    
     }
-    else if empty_string_zero == "M" {
-        println!("You picked -- {}", empty_string_zero);
+    else if digit_zero == "M" {
+        println!("You picked -- {}", digit_zero);
         let op_result_multiply = calc_multiply(digit_one, digit_two);
         println!("multiply: Result of this operation is: {}", op_result_multiply);    
     }
-    else if empty_string_zero == "D" {
-        println!("You picked -- {}", empty_string_zero);
+    else if digit_zero == "D" {
+        println!("You picked -- {}", digit_zero);
         let op_result_divide = calc_divide(digit_one, digit_two);
         println!("divide: Result of this operation is: {}", op_result_divide);    
     }
-    else if empty_string_zero == "E" {
-        println!("You picked -- {}", empty_string_zero);
+    else if digit_zero == "E" {
+        println!("You picked -- {}", digit_zero);
     }
     else {
         println!("You picked an unavailable choice");
